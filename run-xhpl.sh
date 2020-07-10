56c9535e32224700fb513e1978af8980b2c239f1
jefflantz <drakondude1212@gmail.com>
2020-07-10 18:22:06 +0000

    runscript no longer needs allows root

--- run-xhpl.sh
+++ run-xhpl.sh
@@ -1,1 +1,1 @@
-nix-shell /nixenv/nixuser/dev.nix --run "cd /home/nixuser; mpirun -np 4 --allow-run-as-root xhpl HPL.dat"
+nix-shell /nixenv/nixuser/dev.nix --run "cd /home/nixuser; mpirun -np 4 xhpl HPL.dat"
